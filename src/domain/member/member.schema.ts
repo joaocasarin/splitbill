247749d62b9e6d1126ceb7c75e5683101291b14f
João Vitor Casarin <joaovitorcasarin@hotmail.com>
2026-03-17 00:25:07 -0300

    refactor: add MemberSchema

---
+++ src/domain/member/member.schema.ts
@@ -0,0 +1,17 @@
+import z from "zod";
+import { EntityIdSchema, USER_NAME_MAX, USER_NAME_MIN } from "../common";
+
+export const MemberSchema = z.object({
+    id: EntityIdSchema,
+    name: z
+        .string()
+        .min(USER_NAME_MIN, {
+            error: `Name must be at least ${USER_NAME_MIN} characters`,
+        })
+        .max(USER_NAME_MAX, {
+            error: `Name cannot exceed ${USER_NAME_MAX} characters`,
+        }),
+    createdAt: z.number().int().positive(),
+});
+
+export type Member = z.infer<typeof MemberSchema>;
